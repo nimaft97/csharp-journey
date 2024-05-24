420be87a9cbc2c58b93cc42f86cbd294eb2b5a7e
nimaft97 <nimafatholahi@gmail.com>
2024-05-23 22:45:22 -0700

    Adding the CardPicker Console App

---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,15 @@
+﻿using PickRandomCards;
+
+Console.Write("Enter a number: ");
+string line = Console.ReadLine();
+if (int.TryParse(line, out int numCards))
+{
+    foreach(var card in CardPicker.PickSomeCards(numCards))
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.WriteLine("Wasn't able to parse the input number");
+}
