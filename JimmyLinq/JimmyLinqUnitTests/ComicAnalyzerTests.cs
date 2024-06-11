a891694679469beb625b1093c2d21a98703535ef
nimaft97 <nimafatholahi@gmail.com>
2024-06-10 17:09:41 -0700

    More tests
    Adding tests to test GetReviews

--- JimmyLinq/JimmyLinqUnitTests/ComicAnalyzerTests.cs
+++ JimmyLinq/JimmyLinqUnitTests/ComicAnalyzerTests.cs
@@ -27,5 +27,55 @@ namespace JimmyLinqUnitTests
             Assert.AreEqual(2, groups.First().First().Issue);
             Assert.AreEqual("Issue 2", groups.First().First().Name);
         }
+
+        [TestMethod]
+        public void ComicAnalyzer_Should_Generate_A_List_Of_Reviews()
+        {
+            var TestReviews = new[]
+            {
+                new Review() { Issue = 1, Critic = Critics.MuddyCritic, Score = 14.5},
+                new Review() { Issue = 1, Critic = Critics.RottenTornadoes, Score = 59.93},
+                new Review() { Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3},
+                new Review() { Issue = 2, Critic = Critics.RottenTornadoes, Score = 95.11},
+            };
+
+            var expectedResults = new[]
+            {
+                "MuddyCritic rated #1 'Issue 1' 14.50",
+                "RottenTornadoes rated #1 'Issue 1' 59.93",
+                "MuddyCritic rated #2 'Issue 2' 40.30",
+                "RottenTornadoes rated #2 'Issue 2' 95.11",
+            };
+
+            var actualResults = ComicAnalyzer.GetReviews(testComics, TestReviews).ToList();
+            CollectionAssert.AreEqual(expectedResults, actualResults);
+        }
+
+        [TestMethod]
+        public void ComicAnalyzer_Should_Handle_Weird_Review_Scores()
+        {
+            var testReviews = new[]
+            {
+                new Review() { Issue = 1, Critic = Critics.MuddyCritic, Score = -12.1212},
+                new Review() { Issue = 1, Critic = Critics.RottenTornadoes, Score = 391691234.48931},
+                new Review() { Issue = 2, Critic = Critics.RottenTornadoes, Score = 0},
+                new Review() { Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3},
+                new Review() { Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3},
+                new Review() { Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3},
+                new Review() { Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3},
+            };
+            var expectedResults = new[]
+            {
+                "MuddyCritic rated #1 'Issue 1' -12.12",
+                "RottenTornadoes rated #1 'Issue 1' 391691234.49",
+                "RottenTornadoes rated #2 'Issue 2' 0.00",
+                "MuddyCritic rated #2 'Issue 2' 40.30",
+                "MuddyCritic rated #2 'Issue 2' 40.30",
+                "MuddyCritic rated #2 'Issue 2' 40.30",
+                "MuddyCritic rated #2 'Issue 2' 40.30",
+            };
+            var actualResults = ComicAnalyzer.GetReviews(testComics, testReviews).ToList();
+            CollectionAssert.AreEqual(expectedResults, actualResults);
+        }
     }
 }
